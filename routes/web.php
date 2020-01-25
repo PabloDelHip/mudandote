7b1c1f19c78141f411787ed7774f509e02d728a1
PabloDelHip <Pablodelhip@gmail.com>
2020-01-25 14:48:47 -0600

    Commit inicial de Laravel sobre Heroku

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,22 @@
 |
 */
 
-Route::get('/', function () {
-    echo 'Hola como';
-});
+// Route::get('/', function () {
+//     echo 'hola';
+// });
+
+// Route::get('/', ['as' => '/', 'uses' => 'TaskController@index']);
+Route::get('/', ['as' => '/', 'uses' => 'CotizacionesController@index']);
+
+
+// Route::resource('tasks', 'TaskController');
+
+// Auth::routes();
+
+// //Login
+// Route::get('/ingresar', ['as' => 'ingresar', 'uses' => 'HomeController@index']);
+// Route::post('/login', ['as' => 'login', 'uses' => 'HomeController@login']);
+
+// // Cotizaciones
+// Route::get('/cotizaciones', ['as' => 'cotizaciones', 'uses' => 'CotizacionesController@index']);
+// Route::post('/nuevo_presupuesto', ['as' => 'nuevo_presupuesto', 'uses' => 'CotizacionesController@nuevoPresupuesto']);
